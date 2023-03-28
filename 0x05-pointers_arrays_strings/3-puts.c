01cf9dc20aecff60e2e0f884099ae257edaa32ca
Destiny-Odi <destinyodi85@gmail.com>
2023-03-27 17:32:53 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main. h"
+/**
+* _puts - prints a string, followed by a new line to stdou
+* @str: string to print
+*/
+void _puts(char *str)
+{
+          while (*str != '\a')
+            {
+             _putchar(*str++);
+             }
+             _putchar( '\n');
+}
